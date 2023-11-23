7c3bd08db28c86ba5b992f8b65e81392f702ca38
OsimFavour <famos204@gmail.com>
2023-11-23 11:26:40 +0100

    Use Debounce Hook

--- userinterface/src/App.js
+++ userinterface/src/App.js
@@ -1,48 +1,58 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Axios from 'axios'
 import './App.css'
+import useDebounce from './hooks/use-debounce'
 
 const url = 'http://127.0.0.1:8000/api'
 
 function App() {
+  const [data, setData] = useState([])
+  const [searchText, setSearchText] = useState('')
+
+  const debounce = useDebounce(searchText, 500)
 
   useEffect(() => {
     fetchData()
-  }, [])
-
-  const fetchData = async () => {
-    try {
-      const response = await Axios.get(
-        `${url}/posts/`
-      )
-      console.log(response.data)
-      
-    }
-    catch(e) {
-      console.log(e.response)
-    }
-  }
+  }, [debounce])
 
   // const fetchData = async () => {
-  //   const endpoint = `${url}/posts/`
-
   //   try {
-  //     const response = await fetch(endpoint, {
-  //       method: 'GET'
-  //     })
-  //     const data = await response.json()
-  //     console.log(data)
+  //     const response = await Axios.get(
+  //       `${url}/posts/`
+  //     )
+  //     console.log(response.data)
+      
   //   }
   //   catch(e) {
-  //     console.log(e)
+  //     console.log(e.response)
   //   }
   // }
 
+  const fetchData = async () => {
+    const endpoint = `${url}/posts?title=${searchText}`
+
+    try {
+      const response = await fetch(endpoint, {
+        method: 'GET'
+      })
+      const data = await response.json()
+      console.log(data)
+      setData(data)
+    }
+    catch(e) {
+      console.log(e)
+    }
+  }
+
   return (
     <div className="App">
-      <input type='search' placeholder='search...' />
+      <input 
+        type='search' 
+        placeholder='search...'
+        value={searchText}
+        onChange={e => setSearchText(e.target.value)} />
       <ul>
-        <li>Test Text</li>
+        {data && data.map((el) => <li key={el.id}>{el.title}</li>)}
       </ul>
     </div>
   );
