5bc2ffa41a45682f3f992e1c00dd155a65cef3c5
OsimFavour <famos204@gmail.com>
2023-11-23 11:27:30 +0100

    Add search query set

--- search_x/prime/api/views.py
+++ search_x/prime/api/views.py
@@ -3,6 +3,15 @@ from .serializers import PostSerializer
 from ..models import Post
 
 
+# http://.../api/posts?title=xxx
+
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def get_queryset(self):
+        qs = Post.objects.all()
+        title = self.request.query_params.get('title')
+        if title is not None:
+            qs = qs.filter(title__icontains=title)
+        return qs    
